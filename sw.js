91d7a3c4d6e1d741b0bee574f6614ff8fc0bdcef
Diana Crisan <crisanmonica18@stud.ase.ro>
2021-01-14 11:08:32 +0200

    solved static files issue

--- sw.js
+++ sw.js
@@ -3,7 +3,31 @@ var staticCacheName = 'office-assistant';
 self.addEventListener("install", function (e) { 
     e.waitUntil( 
       caches.open(staticCacheName).then(function (cache) { 
-        return cache.addAll(["/"]); 
+        return cache.addAll([
+          './JS files/form-submit.js',
+          './JS files/img-roll-sound.js',
+          './JS files/index.js',
+          './JS files/modal-add-activity.js',
+          './JS files/nav-sound.js',
+          './JS files/slider.js',
+          './JS files/speech-recognition.js',
+          './JS files/weekly-stats.js',
+          './JS files/your-activities.js',
+          './media/images/diana.jpg',
+          './media/images/nick.jpeg',
+          './media/images/smoto.jpeg',
+          './media/images/header-background.jpg',
+          './media/images/logo.png',
+          './media/sounds/clicksound.mp3',
+          './JS filesmedia/sounds/clicksound.mp3',
+          './media/sounds/dropSound.mp3',
+          './media/sounds/imgRollSound.mp3',
+          './media/videos/sample_contact_us.mp4',
+          './media/videos/sample_woman_office.mp4',
+          './Styles/querries.css',
+          './Styles/style.css',
+          './index.html'
+        ]); 
       }) 
     ); 
   }); 
